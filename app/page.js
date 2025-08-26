d97362f841cc1a3f4f056e88a3dcd5dc88d3400f
Tanushree Gupta <tanugupta1004@gmail.com>
2025-08-26 22:21:10 +0530

    added hero-bg

--- app/page.js
+++ app/page.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 import NavBar from "./components/NavBar";
 import Scan from "./components/Scan";
 import Blog from "./components/Blog";
+import Hero from "./components/Hero";
+
 
 export default function Home() {
   return (
     <div>
       <NavBar/>
+      <Hero/>
       <Scan/>
       <Blog 
         name="The Rise of Cybersecurity in the Digital Age" 

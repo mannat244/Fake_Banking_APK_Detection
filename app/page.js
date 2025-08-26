7ee9af895a24976903cacfe66355713aa99e5650
mannat244 <mecommunitymail@gmail.com>
2025-08-27 01:53:02 +0530

    scan comp removed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import NavBar from "./components/NavBar";
-import Scan from "./components/Scan";
 import Blog from "./components/Blog";
 import Hero from "./components/Hero";
 
@@ -10,7 +9,6 @@ export default function Home() {
     <div>
       <NavBar/>
       <Hero/>
-      <Scan/>
       <Blog 
         name="The Rise of Cybersecurity in the Digital Age" 
         details="Cybersecurity is increasingly vital as digital threats evolve. Organizations must adopt robust measures to protect sensitive data and ensure safe online experiences."

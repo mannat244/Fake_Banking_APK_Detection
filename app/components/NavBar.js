dcefca99fb8702f7c7e4968560ad148cffe7816e
Tanushree Gupta <tanugupta1004@gmail.com>
2025-08-30 13:01:48 +0530

    updated logo

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -5,7 +5,7 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full h-16 z-50 flex items-center justify-between px-6 border-b border-[#0e1b2e4d] backdrop-blur-lg bg-[#061018]/80">
-      {/* Logo Section */}
+      
       <div className="flex items-center">
         <img
           src="/designs/logo.png"
@@ -14,7 +14,7 @@ const NavBar = () => {
         />
       </div>
 
-      {/* Links Section */}
+      
       <div className="flex gap-8 items-center mr-64">
         <a href="#home" className="text-white hover:text-[#47a4d7] font-medium transition duration-300">Home</a>
         <a href="#about" className="text-white hover:text-[#47a4d7] font-medium transition duration-300">Services</a>
